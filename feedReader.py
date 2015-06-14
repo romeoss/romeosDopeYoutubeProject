b3c41eb342729a156a2066e58193d66202116354
romeoss <romeo.s.solano@gmail.com>
2015-06-14 08:35:52 -0700

    Tried setting everything to str. Didn't help. Really confused as to why it doesn't find things in history even though videoURL occurs within historyList. Perhaps 'not in' is being used incorrectly? Perhaps it's still an encoding problem? Not sure. Sleepy. Have fun Gamah.

--- feedReader.py
+++ feedReader.py
@@ -1,61 +1,58 @@
-import feedparser #This will make life easiehttps://i.imgur.com/k79LuE1.pngr
+import feedparser                                                                                     #This will make life easier
 
-class color: #These make things pretty
+class color:                                                                                          #These make things pretty
 	YELLOW = '\033[93m'
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 	END = '\033[0m'
 
-posts = 5 #Maximum number of recent videos that will be read
-feeds = "feeds.txt" #Example: https://www.youtube.com/feeds/videos.xml?channel_id=UC_ZSVTRRC5p3ed6hu8Y8z7A
-history = "history.txt" #NEEDS AT LEAST ONE LINE TO START OUT
-
+posts = 5                                                                                             #Maximum number of recent videos that will be read
+feeds = "feeds.txt"                                                                                   #Example: https://www.youtube.com/feeds/videos.xml?channel_id=UC_ZSVTRRC5p3ed6hu8Y8z7A
+history = "history.txt"                                                                               #NEEDS AT LEAST ONE LINE TO START OUT
 
 
 ##THIS IS THE PART WHERE THE HISTORY IS LOADED INTO THE LIST historyList
-file = open(history) #Opening this so that we can read the history
-historyList = [] #Creating lists needed to document history
+file = open(history)                                                                                  #Opening this so that we can read the history
+historyList = []                                                                                      #Creating lists needed to document history
 newHistory = []
 with file as g:
 	for line in g:
 		url = line
-		historyList+=[url] #Take each line from history.txt and write it to an element in historyList
-file.close() #Close the file so that we can open the next one
+		historyList+=[url]                                                                    #Take each line from history.txt and write it to an element in historyList
+file.close()                                                                                          #Close the file so that we can open the next one
 
 
 ##THIS IS THE PART WHERE THE SHIT IS PRINTED
-file = open(feeds)#Open the feeds.txt so that we can go through it and get each channel's feed
+file = open(feeds)                                                                                    #Open the feeds.txt so that we can go through it and get each channel's feed
 with file as f:
 	for line in f:
 		i = 0
-		url = line#Each line is a url that we will process
-		parsedfeed = feedparser.parse(url)#Actually getting the RSS feed itself using the URL
-		print color.YELLOW + color.BOLD + parsedfeed.channel.title + color.END#Displaying the channel name in a pretty way.
+		url = line                                                                            #Each line is a url that we will process
+		parsedfeed = feedparser.parse(url)                                                    #Actually getting the RSS feed itself using the URL
+		print color.YELLOW + color.BOLD + parsedfeed.channel.title + color.END                #Displaying the channel name in a pretty way.
 		while i < posts:
-			#print newHistory
-			#print historyList
-			videoTitle = parsedfeed.entries[i].link + "\\n"
-			if videoTitle not in historyList:#Check to see if the video link is in the historyList
-				print color.BOLD + parsedfeed.entries[i].title + color.END#Print the name of the video
-				print color.UNDERLINE + parsedfeed.entries[i].link + color.END#Print the link to the video
-				#print "VIDEO NOT FOUND IN HISTORY"
-				newHistory+=[parsedfeed.entries[i].link]#Add the video's link to the list that will be written to history.txt later
-				i+=1
-				#print i
-			else:
-				i = posts
-				#print i
-				#print "VIDEO FOUND IN HISTORY"
-
-		print ""#Put a newline between channels so that the link to Garcatch's latest video doesn't touch ViHart's name
-file.close()#Close the feeds file so that we can open history back up. 
+			print "VIDEO LINK: " + parsedfeed.entries[i].link
+			print "CONTENTS OF historyList: ",
+			print historyList
+			print "CONTENTS OF newHistory: ",
+			print newHistory
+			videoURL = parsedfeed.entries[i].link + "\\n"                                 #We need to do this b/c historyList loads the newline characters and we want to match to that. Alternative solution: Remove the newline character from each entry in historyList
+			print videoURL
+			newHistory+=[parsedfeed.entries[i].link]
+			if videoURL not in historyList:                                               #Check to see if the video link is in the historyList
+#				print color.BOLD + parsedfeed.entries[i].title + color.END            #Print the name of the video
+#				print color.UNDERLINE + parsedfeed.entries[i].link + color.END        #Print the link to the video
+				print "VIDEO NOT FOUND IN HISTORY\n\n\n"
+			i+=1
+		print ""                                                                              #Put a newline between channels so that the link to Garcatch's latest video doesn't touch ViHart's name
+file.close()                                                                                          #Close the feeds file so that we can open history back up. 
 
 
 ##THIS IS THE PART WHERE SHIT GETS ADDED TO history.txt
-file = open(history, 'a')#Open history in writable mode so that we can add to it. 
+file = open(history, 'a')                                                                             #Open history in writable mode so that we can add to it. 
 with file as h:
-	for m in range(0, len(newHistory)):#For each element in newHistory
-		h.write(newHistory[m])#Write that element to history.txt
-		h.write("\n")#Put a newline so that they can be read by historyList next time
+	for m in range(0, len(newHistory)):                                                           #For each element in newHistory
+		h.write(newHistory[m])                                                                #Write that element to history.txt
+		h.write("\n")                                                                         #Put a newline so that they can be read by historyList next time
 ##TODO: DON'T ADD THE URL TO HISTORY IF IT'S ALREADY IN THERE
-file.close()#Close history.txt like a good Python skiddie
+file.close()                                                                                          #Close the file
