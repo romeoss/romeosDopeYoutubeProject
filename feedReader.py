c59fda877994d826c4664309ebbf7225630a600d
romeoss <romeo.s.solano@gmail.com>
2015-06-14 10:38:55 -0700

    Fixed bug where script would save videos to history that were already in history. Also added emotions

--- feedReader.py
+++ feedReader.py
@@ -4,6 +4,7 @@ import feedparser                                                               
 class color:                                                                                          #These make things pretty
 	YELLOW = '\033[93m'
 	RED = '\033[91m'
+	BLUE = '\033[94m'
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 	END = '\033[0m'
@@ -33,6 +34,7 @@ with file as feedsFile:
 		url = line                                                                            #Each line is a url that we will process
 		parsedfeed = feedparser.parse(url)                                                    #Actually getting the RSS feed itself using the URL
 		print color.YELLOW + color.BOLD + parsedfeed.channel.title + color.END                #Displaying the channel name in a pretty way.
+		atLeastOneVideo = "nope"
 		while i < posts:
 			videoURL = parsedfeed.entries[i].link + "\n"                                  #We need to do this b/c historyList loads the newline characters and we want to match to that. Alternative solution: Remove the newline character from each entry in historyList
 			videoURL = videoURL.encode('ascii', 'ignore')
@@ -40,7 +42,10 @@ with file as feedsFile:
 			if videoURL not in historyList:                                               #Check to see if the video link is in the historyList
 				print color.BOLD + parsedfeed.entries[i].title + color.END            #Print the name of the video
 				print color.UNDERLINE + parsedfeed.entries[i].link + color.END        #Print the link to the video
+				atLeastOneVideo = "yeah"
 			i += 1
+		if atLeastOneVideo == "nope":
+			print color.BLUE + "No new videos from " + parsedfeed.channel.title + " :'(" + color.END
 		print ""                                                                              #Put a newline between channels so that the link to Garcatch's latest video doesn't touch ViHart's name
 file.close()                                                                                          #Close the feeds file so that we can open history back up. 
 
@@ -49,7 +54,9 @@ file.close()                                                                    
 file = open(history, 'a')                                                                             #Open history in writable mode so that we can add to it. 
 with file as historyFile:
 	for m in range(0, len(newHistory)):                                                           #For each element in newHistory
-		if newHistory[m] not in historyList:
+		videoURL = newHistory[m] + "\n"
+		videoURL = videoURL.encode('ascii', 'ignore')
+		if videoURL not in historyList:
 			historyFile.write(newHistory[m])                                              #Write that element to history.txt
 			historyFile.write("\n")                                                       #Put a newline so that they can be read by historyList next time
 file.close()                                                                                          #Close the file
