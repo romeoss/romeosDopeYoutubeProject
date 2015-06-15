d5dea8bad9c529942f73d8f126c79e97a6fc6312
romeoss <romeo.s.solano@gmail.com>
2015-06-14 23:45:27 -0700

    fixed all known bugs and removed debug code. Program is now fully in python3

--- feedReader.py
+++ feedReader.py
@@ -34,23 +34,15 @@ with file as feedsFile:
 		print(color.YELLOW + color.BOLD + parsedfeed.channel.title + color.END)               #Displaying the channel name in a pretty way.
 		atLeastOneVideo = "nope"
 		while i < posts:
-			videoURL = parsedfeed.entries[i].link + "\\n"                                  #We need to do this b/c historyList loads the newline characters and we want to match to that. Alternative solution: Remove the newline character from each entry in historyList
-			print(videoURL),
-			print(historyList),
-			#print(newHistory)
+			videoURL = parsedfeed.entries[i].link + '\n'                                  #We need to do this b/c historyList loads the newline characters and we want to match to that. Alternative solution: Remove the newline character from each entry in historyList
 			newHistory += [parsedfeed.entries[i].link]
-			#print(isinstance(videoURL, str))
-			#print(isinstance(historyList[1], str))
 			if videoURL not in historyList:                                               #Check to see if the video link is in the historyList
-#				print(color.BOLD + parsedfeed.entries[i].title + color.END)           #Print the name of the video
-#				print(color.UNDERLINE + parsedfeed.entries[i].link + color.END)       #Print the link to the video
+				print(color.BOLD + parsedfeed.entries[i].title + color.END)           #Print the name of the video
+				print(color.UNDERLINE + parsedfeed.entries[i].link + color.END)       #Print the link to the video
 				atLeastOneVideo = "yeah"
-				print("not in list")
-			#if videoURL == historyList[1]:
-			#	print("HEY LOOK AT THIS BRAH LASDFLASDLKFASDKFKASDFKASDFHKLASDHFKLASDHKLHF?")
 			i += 1
-#		if atLeastOneVideo == "nope":
-#			print(color.BLUE + "No new videos from " + parsedfeed.channel.title + " :'(" + color.END)
+		if atLeastOneVideo == "nope":
+			print(color.BLUE + "No new videos from " + parsedfeed.channel.title + " :(" + color.END)
 		print("")                                                                              #Put a newline between channels so that the link to Garcatch's latest video doesn't touch ViHart's name
 file.close()                                                                                          #Close the feeds file so that we can open history back up. 
 
@@ -59,11 +51,12 @@ file.close()                                                                    
 file = open(history, 'a')                                                                             #Open history in writable mode so that we can add to it. 
 with file as historyFile:
 	for m in range(0, len(newHistory)):                                                           #For each element in newHistory
-		videoURL = newHistory[m] + "\n"
+		videoURL = newHistory[m] + '\n'
 		if videoURL not in historyList:
 			historyFile.write(newHistory[m])                                              #Write that element to history.txt
-			historyFile.write("\n")                                                       #Put a newline so that they can be read by historyList next time
+			historyFile.write('\n')                                                       #Put a newline so that they can be read by historyList next time
 file.close()                                                                                          #Close the file
 
-print(color.RED + "THANK YOU FOR USING ROMEO'S DOPE YOUTUBE PROJECT!")
-print("==IT WAS MADE WITH HELP FROM ~GAMAH AND MITFREE==" + color.END)
+print(color.RED + "=======THANK YOU FOR USING ROMEO'S DOPE YOUTUBE PROJECT=======")
+print("==MADE WITH HELP FROM GAMAH, MITFEE,ANDERSON, AND DARDEVELIN==")
+print("============THIS SOFTWARE CARRIES THE MIT LICENSE=============" + color.END)
